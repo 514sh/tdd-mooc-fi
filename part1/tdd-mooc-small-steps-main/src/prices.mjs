d7243370b1b33f94480496a44bd05f94348c4761
514sh <mark.joseph.balagtas@outlook.com>
2023-08-13 07:18:45 +0800

    tcr: tests pass

--- part1/tdd-mooc-small-steps-main/src/prices.mjs
+++ part1/tdd-mooc-small-steps-main/src/prices.mjs
@@ -77,7 +77,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && isMonday(date, date2) && !isHoliday(date)) {
+    if (date && isMonday(date, date2) && !isHoliday(date, date2)) {
       reduction = 35;
     }
     return reduction;
@@ -87,7 +87,7 @@ function createApp(database) {
     return date.getDay() === 1;
   }
 
-  function isHoliday(date) {
+  function isHoliday(date, date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
