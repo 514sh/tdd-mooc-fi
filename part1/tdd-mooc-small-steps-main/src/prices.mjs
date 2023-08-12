b60fccd3f9852c4e03d01d851c07d28b91203f1c
514sh <mark.joseph.balagtas@outlook.com>
2023-08-13 07:46:20 +0800

    tcr: tests pass

--- part1/tdd-mooc-small-steps-main/src/prices.mjs
+++ part1/tdd-mooc-small-steps-main/src/prices.mjs
@@ -25,9 +25,6 @@ function createApp(database) {
   });
 
   function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString);
-    }
   }
 
   function parsePlainDate(dateString){
