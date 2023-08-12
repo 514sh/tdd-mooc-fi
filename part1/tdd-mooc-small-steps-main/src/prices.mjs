fef02a09f785225d8a2fcba3ded8ee2d663c3450
514sh <mark.joseph.balagtas@outlook.com>
2023-08-13 07:29:06 +0800

    tcr: tests pass

--- part1/tdd-mooc-small-steps-main/src/prices.mjs
+++ part1/tdd-mooc-small-steps-main/src/prices.mjs
@@ -94,7 +94,6 @@ function createApp(database) {
       let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
         date &&
-        date.getFullYear() === holiday.getFullYear() &&
         date2 && 
         date2.year === holiday2.year &&
         date2.month === holiday2.month &&
