36d11bbb4c276b22d69715414430c9b71ee96eb8
514sh <mark.joseph.balagtas@outlook.com>
2025-02-06 17:41:24 +0800

    run lint

--- part3/src/untestable1.mjs
+++ part3/src/untestable1.mjs
@@ -10,18 +10,3 @@ export function daysUntilChristmas() {
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
-
-
-export function testableDaysUntilChristmas(dateCompared){
-  if (!dateCompared){
-    dateCompared = new Date();
-  }
-  const december = 12 - 1;
-  const comparedYear = dateCompared.getFullYear();
-  const christmasDay = new Date(comparedYear, december, 25);
-  if(dateCompared.getTime() > christmasDay.getTime()){
-    christmasDay.setFullYear(comparedYear + 1);
-  }
-  const diffMillis = christmasDay.getTime() - dateCompared.getTime();
-  return Math.floor(diffMillis/ millisPerDay);
-}
